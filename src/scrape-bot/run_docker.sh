cbec7c9f59615ef2537fcfc15b1c25e9092fba7b
johnny.nguyen541@gmail.com <johnny.nguyen541@gmail.com>
2022-07-01 00:27:31 -0700

    Created Scrape API

---
+++ src/scrape-bot/run_docker.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+## Variables
+BUILD_NUMBER=""
+TAG_ID="scrape-bot"
+APP_PORT=5002
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+docker build --tag=$TAG_ID .
+
+# Step 2: 
+# List docker images
+docker image ls
+
+# Step 3: 
+# Run flask app - Print if successful
+if BUILD_NUMBER=$(docker run -d -p $APP_PORT:$APP_PORT $TAG_ID)
+then
+    echo "Successfully built $BUILD_NUMBER"
+    echo "Successfully tagged $TAG_ID"
+else
+    echo "Issue creating Docker image with tag id: $TAG_ID.  Please check output."
+fi
---
+++ src/scrape-bot/upload_docker.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+## Variables
+DOCKER_ID="johnnynguyen541"
+TAG_ID="scrape-bot"
+IMAGE_VERSION="v1.0.0"
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+export DOCKERPATH="$DOCKER_ID/$TAG_ID:$IMAGE_VERSION"
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $DOCKERPATH"
+docker login --username $DOCKER_ID --password "$DOCKER_PASSWORD"
+docker tag $TAG_ID $DOCKERPATH
+
+# Step 3:
+# Push image to a docker repository
+docker push $DOCKERPATH
